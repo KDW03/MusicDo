c91c39e917bf1a1a4aad4920764315324520cbe2
kimdowoo <asasa0628@gs.cwnu.ac.kr>
2023-11-20 09:30:20 +0900

    feat: Implement core:media-common Module (#7)

---
+++ core/media-common/src/main/kotlin/com/najudoryeong/musicdo/core/media/common/MediaConstants.kt
@@ -0,0 +1,11 @@
+package com.najudoryeong.musicdo.core.media.common
+
+/**
+ * 미디어 관련 기본 상수 값
+ */
+object MediaConstants {
+    const val DEFAULT_MEDIA_ID = ""
+    const val DEFAULT_INDEX = 0 // 기본
+    const val DEFAULT_POSITION_MS = 0L // 기본 재생 위치
+    const val DEFAULT_DURATION_MS = 0L // 기본 지속 시간
+}

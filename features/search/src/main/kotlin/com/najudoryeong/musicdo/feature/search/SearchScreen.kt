d48c55903d2f21d95b44615dd921d6e2b1625e2a
kimdowoo <asasa0628@gs.cwnu.ac.kr>
2023-11-22 22:06:29 +0900

    feat : Implement SearchScreen & SearchViewModel (#11)

--- features/search/src/main/kotlin/com/najudoryeong/musicdo/feature/search/SearchScreen.kt
+++ features/search/src/main/kotlin/com/najudoryeong/musicdo/feature/search/SearchScreen.kt
@@ -16,9 +16,20 @@
 
 package com.najudoryeong.musicdo.feature.search
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.najudoryeong.musicdo.core.model.MusicState
+import com.najudoryeong.musicdo.core.model.SortBy
+import com.najudoryeong.musicdo.core.model.SortOrder
+import com.najudoryeong.musicdo.core.ui.component.MediaPager
+import com.najudoryeong.musicdo.feature.search.component.SearchTextField
 
 @Composable
 internal fun SearchRoute(
@@ -27,6 +38,89 @@ internal fun SearchRoute(
     onNavigateToAlbum: (Long) -> Unit,
     onNavigateToFolder: (String) -> Unit,
     modifier: Modifier = Modifier,
-    viewModel: SearchViewModel = hiltViewModel(),
+    viewModel: SearchViewModel = hiltViewModel()
 ) {
+    val state by viewModel.uiState.collectAsStateWithLifecycle()
+    val musicState by viewModel.musicState.collectAsStateWithLifecycle()
+
+    when (val uiState = state) {
+        SearchUiState.Loading -> Unit
+        is SearchUiState.Success -> {
+            SearchScreen(
+                modifier = modifier,
+                uiState = uiState,
+                musicState = musicState,
+                onQueryChange = viewModel::changeQuery,
+                onChangeSortOrder = viewModel::onChangeSortOrder,
+                onChangeSortBy = viewModel::onChangeSortBy,
+                onSongClick = { startIndex ->
+                    viewModel.play(startIndex)
+                    onNavigateToPlayer()
+                },
+                onArtistClick = onNavigateToArtist,
+                onAlbumClick = onNavigateToAlbum,
+                onFolderClick = onNavigateToFolder,
+                onPlayClick = {
+                    viewModel.play()
+                    onNavigateToPlayer()
+                },
+                onShuffleClick = {
+                    viewModel.shuffle()
+                    onNavigateToPlayer()
+                },
+                onToggleFavorite = viewModel::onToggleFavorite
+            )
+        }
+    }
 }
+
+
+@Composable
+private fun SearchScreen(
+    uiState: SearchUiState.Success,
+    musicState: MusicState,
+    onQueryChange: (String) -> Unit,
+    onChangeSortOrder: (SortOrder) -> Unit,
+    onChangeSortBy: (SortBy) -> Unit,
+    onSongClick: (Int) -> Unit,
+    onArtistClick: (Long) -> Unit,
+    onAlbumClick: (Long) -> Unit,
+    onFolderClick: (String) -> Unit,
+    onPlayClick: () -> Unit,
+    onShuffleClick: () -> Unit,
+    onToggleFavorite: (id: String, isFavorite: Boolean) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+        SearchTextField(query = uiState.query, onQueryChange = onQueryChange)
+        AnimatedVisibility(
+            visible = uiState.query.isNotBlank(),
+            enter = MediaPagerEnterTransition,
+            exit = MediaPagerExitTransition
+        ) {
+            MediaPager(
+                songs = uiState.searchDetails.songs,
+                currentPlayingSongId = musicState.currentMediaId,
+                artists = uiState.searchDetails.artists,
+                albums = uiState.searchDetails.albums,
+                folders = uiState.searchDetails.folders,
+                sortOrder = uiState.sortOrder,
+                sortBy = uiState.sortBy,
+                onChangeSortOrder = onChangeSortOrder,
+                onChangeSortBy = onChangeSortBy,
+                onSongClick = onSongClick,
+                onArtistClick = onArtistClick,
+                onAlbumClick = onAlbumClick,
+                onFolderClick = onFolderClick,
+                onPlayClick = onPlayClick,
+                onShuffleClick = onShuffleClick,
+                onToggleFavorite = onToggleFavorite
+            )
+        }
+    }
+}
+
+private val MediaPagerEnterTransition = fadeIn()
+private val MediaPagerExitTransition = fadeOut()
+
+
--- features/search/src/main/kotlin/com/najudoryeong/musicdo/feature/search/SearchViewModel.kt
+++ features/search/src/main/kotlin/com/najudoryeong/musicdo/feature/search/SearchViewModel.kt
@@ -16,9 +16,87 @@
 
 package com.najudoryeong.musicdo.feature.search
 
+import android.annotation.SuppressLint
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.najudoryeong.musicdo.core.domain.usecase.GetUserDataUseCase
+import com.najudoryeong.musicdo.core.domain.usecase.SearchMediaUseCase
+import com.najudoryeong.musicdo.core.domain.usecase.SetSortByUseCase
+import com.najudoryeong.musicdo.core.domain.usecase.SetSortOrderUseCase
+import com.najudoryeong.musicdo.core.domain.usecase.ToggleFavoriteSongUseCase
+import com.najudoryeong.musicdo.core.media.common.MediaConstants
+import com.najudoryeong.musicdo.core.media.service.MusicServiceConnection
+import com.najudoryeong.musicdo.core.model.SearchDetails
+import com.najudoryeong.musicdo.core.model.SortBy
+import com.najudoryeong.musicdo.core.model.SortOrder
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@SuppressLint("UnsafeOptInUsageError")
 @HiltViewModel
-class SearchViewModel @Inject constructor() : ViewModel()
+class SearchViewModel @Inject constructor(
+    private val musicServiceConnection: MusicServiceConnection,
+    searchMediaUseCase: SearchMediaUseCase,
+    getUserDataUseCase: GetUserDataUseCase,
+    private val setSortOrderUseCase: SetSortOrderUseCase,
+    private val setSortByUseCase: SetSortByUseCase,
+    private val toggleFavoriteSongUseCase: ToggleFavoriteSongUseCase,
+) : ViewModel() {
+    private val query = MutableStateFlow("")
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private val searchDetails = query
+        .flatMapLatest { query -> searchMediaUseCase(query.trim()) }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.Lazily,
+            initialValue = SearchDetails(
+                songs = emptyList(),
+                artists = emptyList(),
+                albums = emptyList(),
+                folders = emptyList(),
+            ),
+        )
+
+    val uiState = combine(
+        query,
+        searchDetails,
+        getUserDataUseCase(),
+    ) { query, searchDetails, userData ->
+        SearchUiState.Success(
+            query = query,
+            searchDetails = searchDetails,
+            sortOrder = userData.sortOrder,
+            sortBy = userData.sortBy,
+        )
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.Eagerly,
+        initialValue = SearchUiState.Loading,
+    )
+
+    val musicState = musicServiceConnection.musicState
+
+    fun changeQuery(newQuery: String) = query.update { newQuery }
+
+    fun onChangeSortOrder(sortOrder: SortOrder) =
+        viewModelScope.launch { setSortOrderUseCase(sortOrder) }
+
+    fun onChangeSortBy(sortBy: SortBy) = viewModelScope.launch { setSortByUseCase(sortBy) }
+
+    fun play(startIndex: Int = MediaConstants.DEFAULT_INDEX) =
+        musicServiceConnection.playSongs(songs = searchDetails.value.songs, startIndex = startIndex)
+
+    fun shuffle() = musicServiceConnection.shuffleSongs(songs = searchDetails.value.songs)
+
+    fun onToggleFavorite(id: String, isFavorite: Boolean) =
+        viewModelScope.launch { toggleFavoriteSongUseCase(id, isFavorite) }
+}
